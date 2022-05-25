c5f80095bde043e11bbec1ba01638b238a5dbb22
ehddn5252 <sunbi5252@naver.com>
2022-05-25 22:42:13 +0900

    add 관심 등록 토글 변경

--- front/src/components/interestregion/InterestRegionRegister.vue
+++ front/src/components/interestregion/InterestRegionRegister.vue
@@ -1,8 +1,9 @@
 <template>
   <b-container>
-    <b-row>
-      <b-button @click="registerInsert">관심 지역 추가</b-button>
-    </b-row>
+    <span @click="checkIsIn">
+      <b-icon icon="star-fill" v-if="!displayByValue"></b-icon>
+      <b-icon icon="star" v-else></b-icon>
+    </span>
   </b-container>
 </template>
 
@@ -15,6 +16,41 @@ export default {
 
   computed: {
     ...mapState(memberStore, ["userInfo"]),
+    displayByValue: function () {
+      return this.isLike;
+    },
+  },
+
+  watch: {
+    dongCode: function () {
+      console.log("created check");
+      const params = {
+        dongCode: this.dongCode,
+        userId: this.userInfo.userId,
+      };
+      http
+        .get(`/interest/list/check`, { params })
+        .then(({ data }) => {
+          console.log("data?");
+          console.log(data);
+          this.interestLength = data.length;
+          if (data.length == 0) {
+            console.log("data.length == 0");
+            this.isLike = false;
+          } else {
+            console.log("data.length == 1");
+            this.isLike = true;
+          }
+        })
+        .catch((e) => {
+          console.log("e");
+          console.log(e);
+        });
+    },
+  },
+
+  data() {
+    return { interestLength: 1, isLike: false };
   },
   props: {
     sidoCode: String,
@@ -22,46 +58,81 @@ export default {
     dongCode: String,
     areaName: String,
   },
+
   methods: {
-    registerInsert() {
-      console.log("===========");
-      console.log(this.userInfo.userId);
-      console.log(this.sidoCode);
-      console.log(this.sigugunCode);
-      console.log(this.dongCode);
-      console.log(this.areaName);
-      // const params = {
-      //   userId: "ssafy", //this.userInfo.userId
-      //   sidoCode: this.sidoCode,
-      //   sigugunCode: this.sigugunCode,
-      //   dongCode: this.dongCode,
-      //   areaName: this.areaName,
-      // };
+    isLikeCheck() {
+      if (this.interestLength == 0) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+    checkIsIn() {
+      const params = {
+        dongCode: this.dongCode,
+        userId: this.userInfo.userId,
+      };
       if (this.dongCode != null) {
-        let msg = "관심 지역이 등록되었습니다.";
         http
-          .post(`/interest/insert`, {
-            userId: this.userInfo.userId,
-            sidoCode: this.sidoCode,
-            sigugunCode: this.sigugunCode,
-            dongCode: this.dongCode,
-            areaName: this.areaName,
-          })
+          .get(`/interest/list/check`, { params })
           .then(({ data }) => {
-            console.log("data?");
-            console.log(data);
-            this.userInterests = data;
-            alert(msg);
+            this.interestLength = data.length;
+            if (this.interestLength == 0) {
+              console.log("isLike is" + this.isLike);
+              this.registerInsert();
+              this.isLike = false;
+            } else {
+              console.log("isLike is" + this.isLike);
+              this.isLike = true;
+              this.deleteInterest(data[0].interestId);
+            }
           })
           .catch((e) => {
             console.log("e");
             console.log(e);
-            msg = "지역을 동까지 선택해주세요.";
-            alert(msg);
           });
       } else {
-        console.log("dong Code is null");
+        alert("동까지 모두 선택해주세요.");
       }
+    },
+
+    deleteInterest(interestId) {
+      const params = { interestId: interestId };
+      http
+        .delete(`/interest/list`, { params })
+        .then(({ data }) => {
+          let msg = "삭제 처리시 문제가 발생했습니다.";
+          if (data == "success") {
+            msg = "삭제가 완료되었습니다.";
+          }
+          alert(msg);
+        })
+        .catch((error) => {
+          console.log("delete error");
+          console.log(error);
+        });
+    },
+
+    registerInsert() {
+      let msg = "관심 지역이 등록되었습니다.";
+      http
+        .post(`/interest/insert`, {
+          userId: this.userInfo.userId,
+          sidoCode: this.sidoCode,
+          sigugunCode: this.sigugunCode,
+          dongCode: this.dongCode,
+          areaName: this.areaName,
+        })
+        .then(({ data }) => {
+          console.log("data:", data);
+          alert(msg);
+        })
+        .catch((e) => {
+          console.log("e");
+          console.log(e);
+          msg = "지역을 동까지 선택해주세요.";
+          alert(msg);
+        });
     },
   },
 };
